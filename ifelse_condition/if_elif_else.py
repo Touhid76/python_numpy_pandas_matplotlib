432b81fcf80df25b447a04cb1a39396049378040
Touhid76 <itouhid043@gmail.com>
2026-03-15 15:15:27 +0600

    Add files via upload

---
+++ ifelse_condition/if_elif_else.py
@@ -0,0 +1,9 @@
+#conditional statement in python
+
+a=int(input("Enter a number:"))
+if a>0:
+    print("The number is positive.")
+elif a<0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
---
+++ ifelse_condition/nested_ifelse.py
@@ -0,0 +1,11 @@
+#nested ifelse
+
+a=int(input("enter  number"))
+if(a>0):
+    if(a>20):
+        print("the number is greater than 20")
+    else:
+        print("the number is greater than 0 but less than or equal to 20")  
+
+else:
+    print("the number is less than or equal to 0")        
